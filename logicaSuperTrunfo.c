18c71fe83e48276fc62e5e7d858af40f193b6b4b
ViniciosAlvesItaborai <136096433+ViniciosAlvesItaborai@users.noreply.github.com>
2025-11-23 05:20:20 +0000

    carta1

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,6 +8,23 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
+    
+    /*Na aprimeira étapa do jogo, iremos criar as vairaveis*/
+
+    int carta1 = 550.000;
+    int carta2 = 450.000;
+    /*Na segunda étapa do jogo, iremos criar os valores*/
+
+    printf("escolha a sua carta\n");
+    scanf("%d", &carta1,carta2 );
+    /*Na terceira étapa do jogo, iremos verificar*/
+
+    if (carta1 < carta2) {   
+        printf(" Rio de janeiro tem menas população \n");
+    }
+    else
+    { printf("Sao paulo tem mais população \n");
+    }
 
     
     // Cadastro das Cartas:
